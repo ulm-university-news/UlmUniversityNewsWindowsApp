f6cf0e00153d2a0a6be6ecda2809c2b78a6cfc0f
Philipp Speidel <ulm.university.news@gmail.com>
2015-11-21 00:25:03 +0100

    Methoden zur Erstellung der Datenbank-Tabellen. Noch nicht vollständig.

--- UlmUniversityNews/DataHandlingLayer/Database/DatabaseManager.cs
+++ UlmUniversityNews/DataHandlingLayer/Database/DatabaseManager.cs
@@ -37,21 +37,10 @@ namespace DataHandlingLayer.Database
                 SQLiteConnection conn = GetConnection();
 
                 // Erstelle LocalUser Tabelle.
-                string sql = @"CREATE TABLE IF NOT EXISTS 
-                            LocalUser   (Id                 INTEGER NOT NULL,
-                                        Name                VARCHAR(35),
-                                        ServerAccessToken   VARCHAR(56),
-                                        PushAccessToken     VARCHAR(1024),
-                                        Platform            INTEGER,
-                                        PRIMARY KEY(Id)
-                            );";
-                using (var statement = conn.Prepare(sql))
-                {
-                    statement.Step();
-                }
+                createLocalUserTable(conn);
 
                 // Schalte Foreign-Key Constraints ein.
-                sql = @"PRAGMA foreign_keys = ON";
+                string sql = @"PRAGMA foreign_keys = ON";
                 using (var statement = conn.Prepare(sql))
                 {
                     statement.Step();
@@ -64,6 +53,167 @@ namespace DataHandlingLayer.Database
             }
             
             Debug.WriteLine("Finished loading the SQLite database.");
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle LocalUser.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createLocalUserTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            LocalUser   (Id                 INTEGER NOT NULL,
+                                        Name                VARCHAR(35) NOT NULL,
+                                        ServerAccessToken   VARCHAR(56),
+                                        PushAccessToken     VARCHAR(1024),
+                                        Platform            INTEGER,
+                                        PRIMARY KEY(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle User.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createUserTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            User    (Id     INTEGER NOT NULL,
+                                    Name    TEXT NOT NULL,
+                                    OldName TEXT,
+                                    PRIMARY KEY(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle Moderator.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createModeratorTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            Moderator   (Id         INTEGER NOT NULL,
+                                        FirstName   TEXT NOT NULL,
+                                        LastName    TEXT NOT NULL,
+                                        Email       TEXT NOT NULL,
+                                        PRIMARY KEY(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle Channel.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createChannelTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            Channel     (Id                 INTEGER NOT NULL,
+                                        Name                TEXT NOT NULL,
+                                        Description         TEXT,
+                                        CreationDate        INTEGER NOT NULL,
+                                        ModificationDate    INTEGER NOT NULL,
+                                        Type                INTEGER NOT NULL,
+                                        Term                TEXT,
+                                        Location            TEXT,
+                                        Dates               TEXT,
+                                        Contact             TEXT,
+                                        Website             TEXT,
+                                        PRIMARY KEY(Id)
+
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle SubscribedChannel.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createSubscribedChannelsTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            SubscribedChannels  (Id         INTEGER NOT NULL,
+                                                Channel_Id  INTEGER NOT NULL,
+                                                PRIMARY KEY(Id, Channel_Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle Lecture.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createLectureTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            Lecture (Channel_Id     INTEGER NOT NULL,
+                                    Faculty         INTEGER NOT NULL,
+                                    StartDate       TEXT,
+                                    EndDate         TEXT,
+                                    Lecturer        TEXT,
+                                    Assistant       TEXT,
+                                    PRIMARY KEY(Channel_Id),
+                                    FOREIGN KEY(Channel_Id) REFERENCES Channel(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle Event.
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createEventTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            Event   (Channel_Id INTEGER NOT NULL,
+                                    Cost        TEXT,
+                                    Organizer   TEXT,
+                                    PRIMARY KEY(Channel_Id),
+                                    FOREIGN KEY(Channel_Id) REFERENCES Channel(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+        /// <summary>
+        /// Erstellt die Tabelle Sports. 
+        /// </summary>
+        /// <param name="conn">Aktive Verbindung zur Datenbank.</param>
+        private static void createSportsTable(SQLiteConnection conn)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS 
+                            Sports  (Channel_Id             INTEGER NOT NULL,
+                                    Cost                    TEXT,
+                                    NumberOfParticipants    TEXT,
+                                    PRIMARY KEY(Channel_Id),
+                                    FOREIGN KEY(Channel_Id) REFERENCES Channel(Id)
+                            );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
         }
     }
 }
