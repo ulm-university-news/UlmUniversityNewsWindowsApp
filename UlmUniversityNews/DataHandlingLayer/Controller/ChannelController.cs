79fd79b48e3ee4fb3d8cf03359110a844ae94a93
Philipp Speidel <ulm.university.news@gmail.com>
2015-12-27 00:36:15 +0100

    Verweis auf ChannelDatabaseManager eingeführt. GetMyChannels Methode implementiert. Testmethode bereitgestellt.

--- UlmUniversityNews/DataHandlingLayer/Controller/ChannelController.cs
+++ UlmUniversityNews/DataHandlingLayer/Controller/ChannelController.cs
@@ -1,4 +1,6 @@
 ﻿using DataHandlingLayer.Controller.ValidationErrorReportInterface;
+using DataHandlingLayer.Database;
+using DataHandlingLayer.DataModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,17 @@ namespace DataHandlingLayer.Controller
     public class ChannelController : MainController
     {
         /// <summary>
+        /// Eine Referenz auf den DatabaseManager, der Funktionalität bezüglich der Kanal-Ressourcen und den zugehörigen Subressourcen beinhaltet.
+        /// </summary>
+        private ChannelDatabaseManager channelDatabaseManager;
+
+        /// <summary>
         /// Erzeugt eine Instanz der ChannelController Klasse.
         /// </summary>
         public ChannelController()
             : base()
         {
-
+            channelDatabaseManager = new ChannelDatabaseManager();
         }
 
         /// <summary>
@@ -25,9 +32,27 @@ namespace DataHandlingLayer.Controller
         public ChannelController(IValidationErrorReport validationErrorReporter)
             : base(validationErrorReporter)
         {
-
+            channelDatabaseManager = new ChannelDatabaseManager();
         }
 
-        
+        /// <summary>
+        /// Liefert eine Liste von Kanälen zurück, die vom lokalen Nutzer abonniert wurden.
+        /// </summary>
+        /// <returns>Eine Liste von Objekten der Klasse Kanal oder einer ihrer Subklassen.</returns>
+        public List<Channel> GetMyChannels()
+        {
+            return channelDatabaseManager.GetSubscribedChannels();
+        }
+
+        // TODO - Remove after testing
+        // Rein für Testzwecke
+        public void storeTestChannel(Channel channel)
+        {
+            // Speichere Kanal in Datenbank.
+            channelDatabaseManager.StoreChannel(channel);
+
+            // Trage Kanal als subscribed ein.
+            channelDatabaseManager.SubscribeChannel(channel.Id);
+        }
     }
 }
