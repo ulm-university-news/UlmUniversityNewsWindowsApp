47fc5374a51a16823116896e88bd530e6e65455d
Philipp Speidel <ulm.university.news@gmail.com>
2016-02-27 22:34:40 +0100

    LoginPage und LoginViewModel hinzugefügt.

---
+++ UlmUniversityNews/DataHandlingLayer/ViewModel/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DataHandlingLayer.ErrorMapperInterface;
+using DataHandlingLayer.NavigationService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandlingLayer.ViewModel
+{
+    public class LoginViewModel : ViewModel
+    {
+        #region Fields
+        #endregion Fields
+
+        #region Properties
+        #endregion Properties
+
+        #region Commands
+        #endregion Commands
+
+        public LoginViewModel(INavigationService navService, IErrorMapper errorMapper)
+            : base(navService, errorMapper)
+        {
+
+        }
+    }
+}
