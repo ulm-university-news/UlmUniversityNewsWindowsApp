03cf7cf31756405eb5f68a9c2460db1442163162
Philipp <philipp.speidel@uni-ulm.de>
2016-06-12 00:09:11 +0200

    Added some API methods for conversations.

--- UlmUniversityNews/DataHandlingLayer/API/GroupAPI.cs
+++ UlmUniversityNews/DataHandlingLayer/API/GroupAPI.cs
@@ -208,5 +208,119 @@ namespace DataHandlingLayer.API
                 serverAccessToken,
                 "/group/" + groupId + "/user/" + participantId);
         }
+
+        // **************** Requests bezüglich Konversationen *****************************************************
+
+        /// <summary>
+        /// Sende einen Request zum Anlegen einer neuen Konversation an den Server.
+        /// Die Konversation wird für die Gruppe angelegt, die durch die angegebene Id identifiziert wird.
+        /// Der Ersteller der Konversation wird automatisch als Konversationsadministrator für diese Konversation festgelegt.
+        /// Der Anfrager muss Teilnehmer der angegebenen Gruppe sein.
+        /// </summary>
+        /// <param name="serverAccessToken">Das Zugriffstoken des Nutzers.</param>
+        /// <param name="groupId">Die Id der Gruppe, für die eine neue Konversation angelegt wird.</param>
+        /// <param name="jsonContent">Die Daten der neuen Konversation in Form eines JSON-Dokuments.</param>
+        /// <returns>Die Antwort des Servers als String. Hier die neu angelegte Konversations-Ressource.</returns>
+        /// <exception cref="APIException">Wenn der Request fehlschlägt, oder vom Server abgelehnt wurde.</exception>
+        public async Task<string> SendCreateConversationRequest(string serverAccessToken, int groupId, string jsonContent)
+        {
+            string serverResponse = await base.SendHttpPostRequestWithJsonBodyAsync(
+                serverAccessToken,
+                jsonContent,
+                "/group/" + groupId.ToString() + "/conversation",
+                null);
+
+            return serverResponse;
+        }
+
+        /// <summary>
+        /// Sende einen Request zum Abfragen aller Konversationen der Gruppe mit der angegebenen Id.
+        /// Der Anfrager muss Teilnehmer der Gruppe sein, um die Abfrage durchführen zu können.
+        /// </summary>
+        /// <param name="serverAccessToken">Das Zugriffstoken des Nutzers.</param>
+        /// <param name="groupId">Die Id der Gruppe, für die die Konversationen abgefragt werden sollen.</param>
+        /// <param name="subresources">Gibt an, ob zu den Konversationsdaten zusätzlich noch die Daten der 
+        ///     Subresourcen abgefragt werden sollen.</param>
+        /// <param name="withCaching">Gibt an, ob für diesen Request Caching erlaubt werden soll.</param>
+        /// <returns>Die Antwort des Servers als String. In diesem Fall eine Auflistung von Kondersations-Ressourcen.</returns>
+        /// <exception cref="APIException">Wenn der Request fehlschlägt, oder vom Server abgelehnt wurde.</exception>
+        public async Task<string> SendGetConversationsRequest(string serverAccessToken, int groupId, bool? subresources, bool withCaching)
+        {
+            Dictionary<string, string> urlParams = new Dictionary<string, string>();
+            if (subresources.HasValue)
+            {
+                urlParams.Add("subresources", subresources.Value.ToString().ToLowerInvariant());
+            }
+
+            string serverResponse = await base.SendHttpGetRequestAsync(
+                serverAccessToken,
+                "/group/" + groupId.ToString() + "/conversation",
+                urlParams,
+                withCaching);
+
+            return serverResponse;
+        }
+
+        /// <summary>
+        /// Sende einen Request zum Abfragen einer spezifischen Konversations-Ressource. Es wird
+        /// die Konversations-Ressource abgefragt, die zu der spezifizierten Gruppe gehört und
+        /// die durch die angegebene Id repräsentiert ist.
+        /// </summary>
+        /// <param name="serverAccessToken">Das Zugriffstoken des Nutzers.</param>
+        /// <param name="groupId">Die Id der Gruppe, zu der die Konversation gehört.</param>
+        /// <param name="conversationId">Die Id der Konversation.</param>
+        /// <param name="withCaching">Gibt an, ob für diesen Request Caching erlaubt werden soll.</param>
+        /// <returns>Die Antwort des Servers als String. Hier die abgefragte Konversations-Ressource.</returns>
+        /// <exception cref="APIException">Wenn der Request fehlschlägt, oder vom Server abgelehnt wurde.</exception>
+        public async Task<string> SendGetConversationRequest(string serverAccessToken, int groupId, int conversationId, bool withCaching)
+        {
+            string serverResponse = await base.SendHttpGetRequestAsync(
+                serverAccessToken,
+                "/group/" + groupId.ToString() + "/conversation/" + conversationId.ToString(),
+                null,
+                withCaching);
+
+            return serverResponse;
+        }
+
+        /// <summary>
+        /// Sendet einen Request zum Ändern einer Konversations-Ressource. Es wird eine Beschreibung
+        /// an durchzuführenden Änderungen an den Server in Form eines JSON-Merge-PATCH Dokuments geschickt.
+        /// Der Anfrager muss der Administrator der Gruppe sein, um diese Anfrage ausführen zu können.
+        /// </summary>
+        /// <param name="serverAccessToken">Das Zugriffstoken des Nutzers.</param>
+        /// <param name="groupId">Die Id der Gruppe, zu der die Konversation gehört.</param>
+        /// <param name="conversationId">Die Id der Konversation.</param>
+        /// <param name="jsonContent">Die Beschreibung der durchzuführenden Änderungen in Form eines
+        ///     Json-Merge PATCH Dokuments.</param>
+        /// <returns>Die Antwort des Servers als String. Hier eine aktualisierte Version der Konversations-Ressource.</returns>
+        /// <exception cref="APIException">Wenn der Request fehlschlägt, oder vom Server abgelehnt wurde.</exception>
+        public async Task<string> SendUpdateConversationRequest(string serverAccessToken, int groupId, int conversationId, string jsonContent)
+        {
+            string serverResponse = await base.SendHttpPatchRequestWithJsonBody(
+                serverAccessToken,
+                jsonContent,
+                "/group/" + groupId.ToString() + "/conversation/" + conversationId.ToString(),
+                null);
+
+            return serverResponse;
+        }
+
+        /// <summary>
+        /// Sende einen Request zum Löschen der Konversation, die durch die angegebene Id eindeutig
+        /// identifiziert wird.
+        /// Der Anfrager muss Gruppenadministrator der Gruppe sein, um diese Anfrage ausführen zu 
+        /// können.
+        /// </summary>
+        /// <param name="serverAccessToken">Das Zugriffstoken des Nutzers.</param>
+        /// <param name="groupId">Die Id der Gruppe, zu der die Konversation gehört.</param>
+        /// <param name="conversationId">Die Id der Konversation.</param>
+        /// <exception cref="APIException">Wenn der Request fehlschlägt, oder vom Server abgelehnt wurde.</exception>
+        public async Task SendDeleteConversationRequest(string serverAccessToken, int groupId, int conversationId)
+        {
+            await base.SendHttpDeleteRequestAsync(
+                serverAccessToken,
+                "/group/" + groupId.ToString() + "/conversation/" + conversationId.ToString());
+        }
     }
 }
